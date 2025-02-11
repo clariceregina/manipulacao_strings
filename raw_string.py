764c84dfe7873e76fe6151783d59e9b1232609a8
Clarice Regina <186510882+clariceregina@users.noreply.github.com>
2025-02-11 08:52:57 -0300

    Add files via upload

---
+++ raw_string.py
@@ -0,0 +1,7 @@
+#1: Criando uma Raw String
+#Crie uma raw string que contenha um caminho de diretório do Windows e imprima o resultado.
+#C:\Users\Nome\Documentos\arquivo.txt
+
+print(r'C:\Users\Nome\Documentos\arquivo.txt')
+
+# A raw string permite interpretar a string exatamente como foi escrita, sem processar caracteres especiais como \n
