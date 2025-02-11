7f96c0192921e5976de4b02edd4e0352fba156b1
Clarice Regina <186510882+clariceregina@users.noreply.github.com>
2025-02-11 07:29:33 -0300

    Adicionando arquivo caracteres de escape

---
+++ caracteres_de_escape.py
@@ -0,0 +1,26 @@
+#1. Exibir aspas dentro de uma string
+#Escreva um código que imprima a seguinte frase exatamente como está: Ela disse: "Python é incrível!"
+
+print('Ela disse: "Python é incrível!"')
+print("Ela disse: \"Pyhton é incrível\"")
+
+#2. Quebra de linha com \n
+#Crie um código que imprima o seguinte texto usando apenas uma string e o caractere de escape correto:
+#Linha 1
+#Linha 2
+#Linha 3
+
+print('Linha 1\nLinha 2\nLinha 3')
+
+#3. Tabulação com \t
+#Crie um código que imprima nome, idade e cidade em forma de tabela
+
+print('Nome\tIdade\tCidade')
+print('Ana\t23\tSão Paulo')
+print('João\t19\tSalvador')
+
+#4. Barra invertida \\
+#Imprima o seguinte caminho de arquivo corretamente em Python:
+#C:\Users\Joao\Documents\arquivo.txt
+
+print('C:\\Users\\Joao\\Documents\\arquivo.txt')
